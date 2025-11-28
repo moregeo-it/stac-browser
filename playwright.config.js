9207c83dcb3ee3c191c0fb0beecf7bd2f8201ed7
Saidy Barry <as.barry@moregeo.it>
2025-11-28 21:15:47 +0100

    ci: configure Playwright to test production in CI and localhost locally.

--- playwright.config.js
+++ playwright.config.js
@@ -27,7 +27,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:8080',
+    baseURL: process.env.PLAYWRIGHT_BASE_URL || 'http://localhost:8080',
     
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -64,7 +64,7 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  webServer: {
+  webServer: process.env.PLAYWRIGHT_BASE_URL ? undefined : {
     command: 'npm start',
     url: 'http://localhost:8080',
     reuseExistingServer: !process.env.CI,
