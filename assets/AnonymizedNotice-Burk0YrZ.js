2fae5dd19beda729f19f2ff6714013e1c45a5951
STAC Browser CI <matthias.mohr@radiant.earth>
2026-01-05 12:10:58 +0000

    deploy: 8b5bf8a023b2601cf420965cf1a6e6038cf668ba

--- assets/AnonymizedNotice-Burk0YrZ.js
+++
@@ -1,3 +0,0 @@
-import{bf as s,v as i,x as c,y as l,E as t,L as m,M as e,b2 as p}from"./index-DLQIbJqc.js";import"./utils-B7F_ho2D.js";import"./_commonjsHelpers-CE1G-McA.js";import"./I18N-DlIXJTel.js";const d={name:"AnonymizedNotice",props:{warning:{type:String,required:!0}}};function _(a,n,o,f,u,y){const r=p;return c(),i(r,{class:"anonymized",variant:"warning",show:""},{default:l(()=>[t("strong",null,e(a.$t("anonymized.title")),1),n[0]||(n[0]=m(` 
-    `,-1)),t("small",null,e(o.warning),1)]),_:1})}const z=s(d,[["render",_]]);export{z as default};
-//# sourceMappingURL=AnonymizedNotice-Burk0YrZ.js.map
